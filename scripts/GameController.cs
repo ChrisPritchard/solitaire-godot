9dd35b06daac82c7bb85bff676785bef0ce9d01c
Chris <chris@grislygrotto.nz>
2026-06-06 15:20:53 +1200

    init new project

---
+++ scripts/GameController.cs
@@ -0,0 +1,12 @@
+using Godot;
+
+namespace SolitaireGodot
+{
+    public partial class GameController : Node
+    {
+        public override void _Ready()
+        {
+            GD.Print("hello from codium!");
+        }
+    }
+}
