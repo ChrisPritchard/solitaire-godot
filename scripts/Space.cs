7417a7df70587157845a07bdea0eb576fb016ad1
Chris <chris@grislygrotto.nz>
2026-06-14 16:15:22 +1200

    fixed dropping stacks on tableau spaces

--- scripts/Space.cs
+++ scripts/Space.cs
@@ -7,11 +7,13 @@ public partial class Space : Sprite2D, ICanParent
 
     public bool CanAccept(Card other)
     {
-        if (Child != null || other.Child != null)
+        if (Child != null)
             return false;
         if (Location == LocationType.Foundation)
-            return other.Rank == 1;
-        return true;
+            return other.Rank == 1 && other.Child == null;
+        if(Location == LocationType.Tableau)
+            return true;
+        return other.Child == null;
     }
     
     public void PositionChild(Card other)
@@ -19,5 +21,7 @@ public partial class Space : Sprite2D, ICanParent
         other.GlobalPosition = GlobalPosition;
         other.ZIndex = ZIndex + 1;
         other.Location = Location;
+        if(other.Child != null)
+            other.PositionChild(other.Child);
     }
 }
